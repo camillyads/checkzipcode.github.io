0807e88c2fa591225c944c9af82eb9bb26d49cd3
Camilly Assis <camilly_ads@outlook.com>
2023-10-21 20:57:12 -0300

    check-zipcode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch} from 'react-icons/fi'
+import './styles.css';
+
+import api from "./services/api";
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch(){
+   // 29112580/json/
+
+   if(input === ''){
+    alert("Preencha o campo!")
+    return;
+   }
+
+   try{
+    const response = await api.get(`${input}/json`);
+    setCep(response.data)
+    setInput("");
+
+   }catch{
+    alert("Ops! Erro ao buscar.");
+    setInput("")
+   }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Check Zip Code</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Type your zip code..."
+        value={input}
+        onChange={(e) => setInput(e.target.value) }
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#fff"/>
+        </button>
+      </div>
+
+
+      {Object.keys(cep).length > 0 && (
+         <main className="main">
+         <h2>Zip Code: {cep.cep}</h2>
+ 
+         <span>{cep.logradouro}</span>
+         <span>{cep.bairro}</span>
+         <span>{cep.localidade} - {cep.uf}</span>
+ 
+       </main>
+      )}
+     
+
     </div>
   );
 }
